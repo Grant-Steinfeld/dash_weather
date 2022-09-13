b8cd9a933fb8f17a6eeabea4bda4d11debf46750
Grant-Steinfeld <grant.steinfeld.tech@gmail.com>
2022-09-13 06:29:43 -0400

    adding x

--- try2.py
+++ try2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import datetime
 
-header=["temp","pressure","humidity","gas","altitude"]
+header=["temp","pressure","humidity","gas","altitude","timestamp"]
 df = pd.read_csv('data_cap/interior_bme688.csv', names=header)
 
 date = str(datetime.datetime.now())
@@ -10,16 +10,21 @@ plt.figure()
 plt.suptitle('run on: ' + date)
 
 plt.subplot(221)
+
 plt.plot(df[['temp']])
+plt.plot(df[['timestamp']])
 
 plt.subplot(222)
 plt.plot(df[['pressure']])
 
+
 plt.subplot(223)
 plt.plot(df[['humidity']])
 
+
 plt.subplot(224)
 plt.plot(df[['gas']])
+
 
 
 plt.savefig("data2.png", format="png")
